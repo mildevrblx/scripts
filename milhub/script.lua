d72a3c128bb4d5e4368a83cc310525ded2f4cf47
mildevrblx <115919945+mildevrblx@users.noreply.github.com>
2022-10-16 03:17:13 -0500

    Update script.lua

--- milhub/script.lua
+++ milhub/script.lua
@@ -1,1 +1,7 @@
+local success = pcall(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/mildevrblx/scripts/main/milhub/games/" .. game.GameId .. ".lua"))();
+end)
 
+if not sucess then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/mildevrblx/scripts/main/milhub/games/universal.lua"))();
+end
